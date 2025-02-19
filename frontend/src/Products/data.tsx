538a6b25e8858f6e7faf709dc046cc06cee48327
Mohammad Jamal <jamal.38@buckeyemail.osu.edu>
2025-02-19 18:44:59 -0500

    finished product component

---
+++ frontend/src/Products/data.tsx
@@ -0,0 +1,48 @@
+export default {
+    products: [
+        {
+            name: 'Shirt',
+            imageUrl: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numberOfReviews: 10,
+        },
+
+        {
+        name: 'Running Shoes',
+        imageUrl: '/images/d2.jpg',
+        price: 120,
+        brand: 'Adidas',
+        rating: 4.7,
+        numberOfReviews: 25,
+    },
+    
+    {
+        name: 'Hoodie',
+        imageUrl: '/images/d3.jpg',
+        price: 80,
+        brand: 'Puma',
+        rating: 4.3,
+        numberOfReviews: 15,
+    },
+
+    {
+        name: 'Backpack',
+        imageUrl: '/images/d4.jpg',
+        price: 50,
+        brand: 'Under Armour',
+        rating: 4.6,
+        numberOfReviews: 20,
+    },
+    
+    {
+        name: 'Smartwatch',
+        imageUrl: '/images/d5.jpg',
+        price: 250,
+        brand: 'Apple',
+        rating: 4.8,
+        numberOfReviews: 40,
+    },
+    ]
+}
