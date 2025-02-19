8ec7fcd4dc7474704bf6cfb9f84f71f90f6ca845
Mohammad Jamal <jamal.38@buckeyemail.osu.edu>
2025-02-19 18:54:54 -0500

    update App.tsx to incorporate the route and update package log to include react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import Menu from "./index";
 import React from "react";
+import Products from "./Products/Products"
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
 import "./index.css";
 
 function App() {
@@ -11,6 +13,9 @@ function App() {
             </header>
             <main>
                 <p>This is a simple website structure.</p>
+                <Routes>
+                    <Route path="/catalog" element={<Products />} />
+                </Routes>
             </main>
             <footer>
                 <p>© 2025 Operation Bus App Team 10 </p>
