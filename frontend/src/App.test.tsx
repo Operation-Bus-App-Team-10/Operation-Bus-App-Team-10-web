1521ecead0aa5d26a488ab2e554393bec500156d
Mohammad Jamal <jamal.38@buckeyemail.osu.edu>
2025-04-05 13:10:38 -0400

    finished simple basic app tests like orders and payment

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('The main app', () => {
+
+    it('should render the Orders link', () => {
+        render(<App />);
+        expect(screen.getByText('Orders')).toBeInTheDocument();
+    });
+
+    it('it should render the Payments component', () => {
+        render(<App />);;
+        expect(screen.getByText('Payment')).toBeInTheDocument();
+      });
+});
