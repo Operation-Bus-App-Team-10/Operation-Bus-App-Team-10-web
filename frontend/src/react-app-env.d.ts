fbe36dcc8bdcf23bf42f2e309db7f120f2046ad3
Ben Dickerson <Dickerson.361@buckeyemail.osu.edu>
2025-04-06 19:13:57 -0400

    sent to github

--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -1,1 +1,8 @@
-/// <reference types="react-scripts" />
+describe('The main app', () => {
+
+    it('should render the Home link', () => {
+        render(<App />); 
+        expect(screen.getByText('Home')).toBeInTheDocument();
+    });
+
+});
